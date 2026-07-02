a2192f64c29ba863ed31feac6183cf157b288911
Etienne Vouga <evouga@gmail.com>
2026-07-01 21:20:31 -0500

    Polyscope duplicate build issue

--- energy-benchmark/cmake/FindPolyscope.cmake
+++ energy-benchmark/cmake/FindPolyscope.cmake
@@ -3,8 +3,31 @@
 #
 #  POLYSCOPE_FOUND - system has POLYSCOPE 
 #  POLYSCOPE_INCLUDE_DIR - **the** POLYSCOPE include directory
+if(TARGET polyscope)
+	if(NOT POLYSCOPE_INCLUDE_DIR)
+		find_path(POLYSCOPE_INCLUDE_DIR polyscope/polyscope.h
+			HINTS
+				ENV POLYSCOPE_DIR
+			PATHS
+				${CMAKE_SOURCE_DIR}/../..
+				${CMAKE_SOURCE_DIR}/..
+				${CMAKE_SOURCE_DIR}
+				${CMAKE_SOURCE_DIR}/polyscope
+				${CMAKE_SOURCE_DIR}/../polyscope
+				${CMAKE_SOURCE_DIR}/../tools/polyscope
+				${CMAKE_SOURCE_DIR}/../../polyscope
+				/usr
+				/usr/local
+			PATH_SUFFIXES include
+		)
+	endif()
+	set(POLYSCOPE_FOUND TRUE)
+	set(Polyscope_FOUND TRUE)
+	return()
+endif()
+
 if(POLYSCOPE_FOUND)
-    return()
+	return()
 endif()
 
 find_path(POLYSCOPE_INCLUDE_DIR polyscope/polyscope.h
@@ -29,4 +52,6 @@ find_package_handle_standard_args(Polyscope
     POLYSCOPE_INCLUDE_DIR)
 mark_as_advanced(POLYSCOPE_INCLUDE_DIR)
 
-add_subdirectory("${POLYSCOPE_INCLUDE_DIR}/../" "polyscope")
+if(NOT TARGET polyscope)
+	add_subdirectory("${POLYSCOPE_INCLUDE_DIR}/../" "polyscope")
+endif()
